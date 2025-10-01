b4a3a84df5fc59e8661889318e5ec207183f64fb
pavieshkumar12 <dhanushpavi22@gmail.com>
2025-10-01 11:26:42 +0530

    api

--- src/controllers/userJournelController.js
+++ src/controllers/userJournelController.js
@@ -1,2 +1,45 @@
 import UserJournel from '../models/userJournelModel.js';
 // import { decryptedDatas } from "../helper/decrypt.js";
+
+
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ message: 'Fields required' });
+    }
+
+    const {
+      contentSummary,
+      contentHandle,
+      contentType,
+    } = req.body;
+
+    // Handle uploaded files (if any)
+    let contentAttachments = [];
+    if (req.files && req.files.length > 0) {
+      contentAttachments = req.files.map(file => file.path); 
+      // OR file.filename if you're storing only the name
+    }
+
+      await UserJournel.create({
+      user: userId,
+      contentSummary,
+      contentHandle,
+      contentType,
+      contentAttachments,
+    });
+
+    res.status(201).json({
+      message: 'User journal created successfully',
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Something went wrong'
+    });
+    next(error);
+  }
+};
+  
--- src/routes/userJournelRoute.js
+++ src/routes/userJournelRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {
-
+    createUserJournel
 } from '../controllers/userJournelController.js'
 import { payloadValidation } from '../helper/playloadValidation.js'
 import { protect } from '../helper/protect.js'
@@ -9,6 +9,12 @@ import { upload } from '../helper/multer.js'
 const router = express.Router()
 
 
+router.post(
+    '/createUserInput',
+    protect,
+    upload.array("contentAttachments", 10), // 10 = max file count (change as needed)
+    createUserJournel
+  )
 
 router.use(errors())
 
